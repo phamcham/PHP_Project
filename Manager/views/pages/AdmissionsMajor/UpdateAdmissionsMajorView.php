143a4d9179bab50f1b8086b61d69f443980013b8
anhthunguyen <59395046+anhthu1242@users.noreply.github.com>
2021-09-01 09:43:25 +0700

    Update UpdateAdmissionsMajorView.php

--- Manager/views/pages/AdmissionsMajor/UpdateAdmissionsMajorView.php
+++ Manager/views/pages/AdmissionsMajor/UpdateAdmissionsMajorView.php
@@ -97,8 +97,10 @@ if (!empty($_POST)) {
                 </div>
 
                 <button class="btn btn-success">Lưu</button>
-                <button class="btn btn-success" onclick="window.location = '/PHP_Project/Manager/AdmissionsMajor'">Trở về </button>
+               
             </form>
+            
+            <button class="btn btn-success" onclick="window.location = '/PHP_Project/Manager/AdmissionsMajor'">Trở về </button>
         </div>
     </div>
 </div>
